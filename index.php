a922c82496c31e56f1a294bdb2a8af4b145f6fba
SamuelArico99 <samuel16699.sa@gmail.com>
2023-03-06 10:51:32 +0100

    Fine esercizio

--- index.php
+++ index.php
@@ -14,8 +14,35 @@ class Movie
         $this->durata = $_durata;
         $this->year = $_year;
     }
+
+    public function printMovie()
+    {
+        echo '<h2>';
+        echo 'TITOLO: ' . $this->title;
+        echo '</h2>';
+
+        echo '<h2>';
+        echo 'GENERE: ' . $this->genere;
+        echo '</h2>';
+
+        echo '<h2>';
+        echo 'DURATA: ' . $this->durata;
+        echo '</h2>';
+
+        echo '<h2>';
+        echo 'ANNO: ' . $this->year;
+        echo '</h2>';
+    }
 }
 
 $dieHard = new Movie('Die Hard', 'Action', '3h', '1990');
 
 var_dump($dieHard);
+
+$dieHard->printMovie();
+
+$jhonWick = new Movie('Jhon Wick 4', 'Action', '2h', '2023');
+
+var_dump($jhonWick);
+
+$jhonWick->printMovie();
