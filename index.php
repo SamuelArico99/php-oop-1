99c36ac3aa7c142705616f87d77e08b4271abc8d
SamuelArico99 <samuel16699.sa@gmail.com>
2023-03-06 10:38:01 +0100

    init

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-class movie
+class Movie
 {
+    public $title;
+    public $genere;
+    public $durata;
+    public $year;
+
+    public function __construct($_title, $_genere, $_durata, $_year)
+    {
+        $this->title = $_title;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+        $this->year = $_year;
+    }
 }
+
+$dieHard = new Movie('Die Hard', 'Action', '3h', '1990');
+
+var_dump($dieHard);
